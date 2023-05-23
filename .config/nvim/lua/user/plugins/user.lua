e3a7684b1803bee6b70a35f2555478bdf8a740fd
Filipe Santiago <porto.santiago@hotmail.com>
2023-05-23 12:40:34 -0300

    add rust tools

--- .config/nvim/lua/user/plugins/user.lua
+++ .config/nvim/lua/user/plugins/user.lua
@@ -73,10 +73,6 @@ return {
         widget_guides = {
           enabled = false,
         },
-        dev_log = {
-          enabled = true,
-          open_cmd = "tabedit",
-        },
         lsp = {
           on_attach = require("astronvim.utils.lsp").on_attach,
         },
@@ -171,5 +167,31 @@ return {
   {
     "stevearc/oil.nvim",
     config = function() require("oil").setup() end,
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    event = { "BufEnter *.rs" },
+    config = function()
+      local rt = require "rust-tools"
+      local mason = require "mason-registry"
+
+      local codelldb = mason.get_package "codelldb"
+      local extension_path = codelldb:get_install_path() .. "/extension/"
+      local codelldb_path = extension_path .. "adapter/codelldb"
+      local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+
+      rt.setup {
+        dap = {
+          adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+        },
+        server = {
+          capabilities = require("astronvim.utils.lsp").capabilities,
+          on_attach = function(_, bufnr)
+            vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+            vim.keymap.set("n", "<leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+          end,
+        },
+      }
+    end,
   },
 }
